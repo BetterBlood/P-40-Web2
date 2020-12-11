63c87a86a5bfbf6dbefe2ed27e20abe76f977d03
Better_Blood <Better_Blood@hotmail.com>
2020-12-11 13:50:05 +0100

    detail

--- view/page/recipe/detail.php
+++ view/page/recipe/detail.php
@@ -19,6 +19,7 @@
 			<?php
 				$imageLink = '"resources/image/Recipes/' . htmlspecialchars($recipe['recImage']) . '"';
 				echo '<img class="d-block w-100" src=' . $imageLink . ' alt="image d\'illustration de la recette">';
+				echo '<p>Description : ' . $recipe['recDescription'] . '</p>';
 			?>
 			<img src="" alt="">
 		</div>
@@ -26,20 +27,18 @@
 		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 text-white">
 			
 			<?php
-			echo '<p>Description : ' . $recipe['recDescription'] . '</p>';
 
 			echo '<div>';
-			if (isset($recipe["recNote"]))
-			{
-				echo '<p>Note : ' . $recipe['recNote'] . '</p>';
-			}
-			else
-			{
-				echo '<p>Note : Il n\'y a aucune note disponible pour le moment</p>'; 
-			}
-			?>
+				if (isset($recipe["recNote"]))
+				{
+					echo '<p>Note : ' . $recipe['recNote'] . '</p>';
+				}
+				else
+				{
+					echo '<p>Note : Il n\'y a aucune note disponible pour le moment</p>'; 
+				}
+				?>
 
-			<div>
 				<!-- Button trigger modal -->
 				<button type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModalCenter">
 					évaluer la recette
@@ -49,30 +48,41 @@
 				<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
 					<div class="modal-dialog modal-dialog-centered" role="document">
 						<div class="modal-content">
-						<div class="modal-header">
-							<h5 class="modal-title" id="exampleModalLongTitle">Évaluation de la recette par <?php echo $user["usePseudo"];?></h5>
-							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-							<span aria-hidden="true">&times;</span>
-							</button>
-						</div>
-						<div class="modal-body">
-							<?php 
-							
-							if ($alreadyRate)
-							{
-								echo 'Votre note est de : ' . $userNote;
-								echo '<select name="note" id="note"></select>';
-								echo '<option value="' . $userNote . '">4</option>';
-								echo '</select>';
-							}
-							
-							
-							?>
-						</div>
-						<div class="modal-footer">
-							<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-							<button type="button" class="btn btn-primary">Save changes</button>
-						</div>
+							<div class="modal-header">
+								<h5 class="modal-title" id="exampleModalLongTitle">Évaluation de la recette par <?php echo $_SESSION["username"];?> </h5>
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+								<span aria-hidden="true">&times;</span>
+								</button>
+							</div>
+							<form action="trouver une page" method="POST">
+								<div class="modal-body">
+									<?php 
+									
+									echo '<select name="note" id="note">';
+									echo '<option value="-1">aucune</option>';
+
+									for ($i = 1; $i <= 5; $i++)
+									{
+										echo '<option';
+										echo ' value="' . $i . '" ';
+
+										if ($alreadyRate && $userNote == $i)
+										{
+											echo 'selected';
+										}
+
+										echo '>'. $i . '</option>';
+									}
+
+									echo '</select>';
+									
+									?>
+								</div>
+								<div class="modal-footer">
+									<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+									<button type="submit" class="btn btn-primary">Save changes</button>
+								</div>
+							</form>
 						</div>
 					</div>
 				</div>
@@ -112,27 +122,26 @@
 			?>
 		</div>
 
-		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-			<div class="text-white">
-				<p>Commentaires d'évaluation : </p>
+		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 text-white">
+			<p>Commentaires d'évaluation : </p>
 
-				<?php
-				
-					foreach($ratings as $rating)
-					{
-						echo '<div class="card text-white bg-secondary  mb-3" style="max-width: 18rem;">';
-							echo '<div class="card-header">' . $rating["usePseudo"] . ', note :' . $rating["ratGrade"] . ' </div>';
-							
-							echo '<div class="card-body">';
-								echo '<h5 class="card-title">' . $rating["ratComment"] . '</h5>';
-							echo '</div>';
-
+			<?php
+			
+				foreach($ratings as $rating)
+				{
+					echo '<div class="card text-white bg-secondary  mb-3" style="max-width: 18rem;">';
+						echo '<div class="card-header">' . $rating["usePseudo"] . ', note :' . $rating["ratGrade"] . ' </div>';
+						
+						echo '<div class="card-body">';
+							echo '<h5 class="card-title">' . $rating["ratComment"] . '</h5>';
 						echo '</div>';
-					}
-				
-				
-				?>
-			</div>
+
+					echo '</div>';
+				}
+			
+			
+			?>
+			
 			
 		</div>
 		<div class="text-white mb-5 pb-5">
