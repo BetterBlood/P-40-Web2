5f15881a2e4eb74724b0d5d5b1f3b15a76088b16
Better_Blood <better_blood@hotmail.com>
2021-01-03 18:14:52 +0100

    DatabaseAndTestCompressedImages
    ajout de ma database et petit test sur la compression des images et notament des gif

--- data/db_web2_recipe2.sql
+++ data/db_web2_recipe2.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Sam 02 Janvier 2021 à 13:34
+-- Généré le :  Dim 03 Janvier 2021 à 17:13
 -- Version du serveur :  5.7.11
 -- Version de PHP :  7.0.3
 
@@ -45,7 +45,8 @@ INSERT INTO `t_rating` (`idRating`, `ratGrade`, `ratComment`, `idRecipe`, `idUse
 (26, 1, 'noComment', 2, 3),
 (27, 3, 'noComment', 4, 3),
 (28, 4, 'noComment', 5, 3),
-(29, 2, 'noCommentxvsdgsdg', 7, 3);
+(29, 2, 'noCommentxvsdgsdg', 7, 3),
+(30, 2, 'bof', 2, 4);
 
 -- --------------------------------------------------------
 
@@ -56,6 +57,7 @@ INSERT INTO `t_rating` (`idRating`, `ratGrade`, `ratComment`, `idRecipe`, `idUse
 CREATE TABLE `t_recipe` (
   `idRecipe` int(11) NOT NULL,
   `recName` varchar(100) NOT NULL,
+  `recCategory` varchar(100) NOT NULL DEFAULT 'pâtisserie',
   `recIngredientList` varchar(255) NOT NULL,
   `recDescription` varchar(255) NOT NULL,
   `recPreparation` varchar(255) NOT NULL,
@@ -71,19 +73,23 @@ CREATE TABLE `t_recipe` (
 -- Contenu de la table `t_recipe`
 --
 
-INSERT INTO `t_recipe` (`idRecipe`, `recName`, `recIngredientList`, `recDescription`, `recPreparation`, `recPrepTime`, `recDifficulty`, `recGrade`, `recImage`, `recDate`, `idUser`) VALUES
-(1, 'recetteParDefault', 'ail, poivre, sel, coucou, 2, r, t, t, t, t, t', 'mélange pas très bon, néanmoins faisable et très facil à réaliser.\r\nBon Appetit !!!!\r\njoyeux Noël', 'tmp,encore tmp,balek, bruler pas trop mais bruler', 1, 2, 3.66667, 'defaultRecipePicture.jpg', '2020-11-13', 1),
-(2, 'defaultRecipe2(easiest)', 'pas grand chose', 'pour le test', 'tmp', 1, 1, 1, 'ingredients-498199_1920.jpg', '2020-11-13', 1),
-(4, 'defaultRecipe4', 'pas grand chose', 'pour le test', 'tmp', 1, 2, 3, 'defaultRecipePicture.jpg', '2020-11-13', 1),
-(5, 'defaultRecipe5', 'pas grand chose', 'pour le test', 'tmp', 1, 2, 4, 'defaultRecipePicture.jpg', '2020-11-13', 1),
-(6, 'defaultRecipe6', 'pas grand chose', 'pour le test', 'tmp', 1, 2, 1, 'defaultRecipePicture.jpg', '2020-11-13', 1),
-(7, 'defaultRecipe7', 'pas grand chose', 'pour le test', 'tmp', 1, 2, 2, 'defaultRecipePicture.jpg', '2020-11-13', 1),
-(8, 'testRecipe8', 'lol', 'idk', 'tmp', 500, 2, 1, 'cook-2364221_1920.jpg', '2020-11-23', 3),
-(9, 'test9bestRecipe', 'pas bcp', 'trop bon', 'tmp', 1, 2, 5, 'egg-943413_1920.jpg', '2020-11-23', 1),
-(10, 'recetteTest10', 'paspaspapspas', 'nobody wanna die', 'tmp', 150, 2, 3, 'cook-2364221_1920.jpg', '2020-11-23', 1),
-(11, 'test', 'test', 'test', 'tmp', 2, 2, 2, 'cook-2364221_1920.jpg', '2020-11-30', 1),
-(12, 'dfwef', 'wefwefwe', 'fwefwef', 'tmp', 0, 2, 2, 'defaultRecipePicture.jpg', '2020-12-03', 2),
-(15, 'nom de la recette', '2 x cornichons,1 x L d\'eau,300 x g. de farine', 'première recette insérée avec le formulaire', 'couper les cornichons en fines lamelles,malaxer les cornichons avec la farine,placer la pâte créé au four pendant 5 heure', 10, 1, NULL, '20210102134517_ingredients-498199_1920.jpg', '0000-00-00', 3);
+INSERT INTO `t_recipe` (`idRecipe`, `recName`, `recCategory`, `recIngredientList`, `recDescription`, `recPreparation`, `recPrepTime`, `recDifficulty`, `recGrade`, `recImage`, `recDate`, `idUser`) VALUES
+(1, 'recetteParDefault', 'pâtisserie', 'ail, poivre, sel, coucou, 2, r, t, t, t, t, t', 'mélange pas très bon, néanmoins faisable et très facil à réaliser.\r\nBon Appetit !!!!\r\njoyeux Noël', 'tmp,encore tmp,balek, bruler pas trop mais bruler', 1, 2, 3.66667, 'defaultRecipePicture.jpg', '2020-11-13', 1),
+(2, 'defaultRecipe2(easiest)', 'pâtisserie', 'pas grand chose', 'pour le test', 'tmp', 1, 1, 1.5, 'ingredients-498199_1920.jpg', '2020-11-13', 1),
+(4, 'defaultRecipe4', 'pâtisserie', 'pas grand chose', 'pour le test', 'tmp', 1, 2, 3, 'defaultRecipePicture.jpg', '2020-11-13', 1),
+(5, 'defaultRecipe5', 'pâtisserie', 'pas grand chose', 'pour le test', 'tmp', 1, 2, 4, 'defaultRecipePicture.jpg', '2020-11-13', 1),
+(6, 'defaultRecipe6', 'pâtisserie', 'pas grand chose', 'pour le test', 'tmp', 1, 2, 1, 'defaultRecipePicture.jpg', '2020-11-13', 1),
+(7, 'defaultRecipe7', 'pâtisserie', 'pas grand chose', 'pour le test', 'tmp', 1, 2, 2, 'defaultRecipePicture.jpg', '2020-11-13', 1),
+(8, 'testRecipe8', 'pâtisserie', 'lol', 'idk', 'tmp', 500, 2, 1, 'cook-2364221_1920.jpg', '2020-11-23', 3),
+(9, 'test9bestRecipe', 'pâtisserie', 'pas bcp', 'trop bon', 'tmp', 1, 2, 5, 'egg-943413_1920.jpg', '2020-11-23', 1),
+(10, 'recetteTest10', 'pâtisserie', 'paspaspapspas', 'nobody wanna die', 'tmp', 150, 2, 3, 'cook-2364221_1920.jpg', '2020-11-23', 1),
+(11, 'test', 'pâtisserie', 'test', 'test', 'tmp', 2, 2, 2, 'cook-2364221_1920.jpg', '2020-11-30', 1),
+(12, 'dfwef', 'pâtisserie', 'wefwefwe', 'fwefwef', 'tmp', 0, 2, 2, 'defaultRecipePicture.jpg', '2020-12-03', 2),
+(15, 'nom de la recette', 'pâtisserie', '2 x cornichons,1 x L d\'eau,300 x g. de farine', 'première recette insérée avec le formulaire', 'couper les cornichons en fines lamelles,malaxer les cornichons avec la farine,placer la pâte créé au four pendant 5 heure', 10, 1, NULL, '20210102134517_ingredients-498199_1920.jpg', '0000-00-00', 3),
+(16, 'test de suppression', 'pâtisserie', '2 x cornichons,4 x L d\'eau,11 x test', 'adasdasdasdasda', 'sadfdfewfwefwef,malaxer les cornichons avec la farine', 11, 1, NULL, 'defaultRecipePicture.jpg', '2021-01-02', 3),
+(18, 'test de suppression', 'pâtisserie', '1 x wrwer', 'sdfsdfe', 'rwwerwer', 13, 1, NULL, 'defaultRecipePicture.jpg', '2021-01-03', 4),
+(28, 'error', 'pâtisserie', '1 x d', 'werwe', 'salut', 10, 1, NULL, 'defaultRecipePicture.jpg', '2021-01-03', 4),
+(29, 'catégorie ?', 'pas pâtisserie', '1 x cornichons', 'yvdgd', 'gdfgdfgf', 10, 1, NULL, '20210103180042_wallpapertip_dark-souls-wallpaper_33023.jpg', '2021-01-03', 4);
 
 -- --------------------------------------------------------
 
@@ -109,7 +115,8 @@ CREATE TABLE `t_user` (
 INSERT INTO `t_user` (`idUser`, `usePseudo`, `useFirstname`, `useName`, `usePassword`, `useMail`, `useTelephone`, `useImage`) VALUES
 (1, 'Dahïr', 'Dahïr', 'Peter', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08', 'jmt-1018@hotmail.com', NULL, 'defaultUserPicture.png'),
 (2, 'test', 'testfirstname', 'testname', 'test', NULL, NULL, 'test.png'),
-(3, 'test2', 'test2', 'test2', 'test2', 'tmp@efwef.com', '0777777777', '20201226135720_775566549163048970.gif');
+(3, 'test2', 'test2', 'test2', 'test2', NULL, NULL, '20210103123527_feu.gif'),
+(4, 'test3', 'test3', 'test3', '$2y$10$dOrWnxEwBJrvkvUWQD7hYuT1StzLdiNfGjRua.szTD0cGPUCho8Vy', NULL, NULL, '20210103181125_test2.png');
 
 --
 -- Index pour les tables exportées
@@ -147,17 +154,17 @@ ALTER TABLE `t_user`
 -- AUTO_INCREMENT pour la table `t_rating`
 --
 ALTER TABLE `t_rating`
-  MODIFY `idRating` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `idRating` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 --
 -- AUTO_INCREMENT pour la table `t_recipe`
 --
 ALTER TABLE `t_recipe`
-  MODIFY `idRecipe` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `idRecipe` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 --
 -- AUTO_INCREMENT pour la table `t_user`
 --
 ALTER TABLE `t_user`
-  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- Contraintes pour les tables exportées
 --
